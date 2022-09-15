95a62f29bb3f77bea38a4a7b5f354f88b3b6e75d
Tinamiyeun <Tinamiyeun@users.noreply.github.com>
2022-09-15 16:42:26 -0400

    initial table management

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,22 @@
 import React, { useContext, useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import MainLayout from "./components/MainLayout";
-import Home from "./components/home/Home";
-import GlobalStyles from "./components/GlobalStyles";
-import Event from "./components/event/Event";
-import AboutUs from "./components/about/AboutUs";
-import EventDetails from "./components/event/EventDetails";
-import UserDashboard from "./components/userDashboard/UserDashboard";
+import MainLayout from "./component/MainLayout";
+import HomePage from "./pages/HomePage";
+import AdminTable from "./pages/AdminTable";
 
 
 function App() {
-
-
-
-  function handleClick(){
-    fetch('diningtable', {
-      method: 'GET',
-    })
-    .then((data) => data.json())
-    .then((json) => {console.log(json);})
-    }
   return (
+    
         <Routes>
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={<Home />}></Route>
-            <Route path="/menu" element={<Menu />}></Route>
+          <Route path="/" element={<HomePage />}>
+          {/*  <Route path="/menu" element={<Menu />}></Route>
             <Route path="/reservation" element={<Reservation />}></Route>
-            <Route path="/shoppingcart" element={<ShoppingCart />}></Route>
+            <Route path="/shoppingcart" element={<ShoppingCart />}></Route>*/}
           </Route>
-          <Route path="/admindashboard" element={<AdminDashboard />}></Route>
-          <Route path="/userdashboard" element={<UserDashboard />}></Route>
+        {/*<Route path="/admindashboard" element={<AdminDashboard />}></Route>*/}
+          <Route path="/admindashboard/table" element={<AdminTable />}></Route>
+          {/*<Route path="/userdashboard" element={<UserDashboard />}></Route>*/}
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
   );
---
+++ frontend/src/pages/AdminTable.js
@@ -0,0 +1,7 @@
+
+import Table from '../component/adminDashboard/Table';
+
+function AdminTable() {
+    return <div><h1>Table management</h1><Table /></div>
+}
+export default AdminTable;
