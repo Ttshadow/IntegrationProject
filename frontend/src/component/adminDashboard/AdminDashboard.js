a5d696cbc117946a45ee3fc653423bc4f83c1b9a
carolinn_lyy <57008592+carolinn-lyy@users.noreply.github.com>
2022-09-15 18:36:51 -0400

    add col and row

--- frontend/src/component/adminDashboard/AdminDashboard.js
+++ frontend/src/component/adminDashboard/AdminDashboard.js
@@ -1,11 +1,20 @@
 import SideBar from './SideBar';
 import Header from '../header/Header';
+import {Container, Col, Row }from 'react-bootstrap';
+
 function AdminDashboard() {
 
     return(
         <>
         <Header />
-        <SideBar />
+        <Container>
+            <Row>
+                <Col>
+                    <SideBar />
+                </Col>
+            </Row>
+        </Container>
+        
         </>
         
     )
