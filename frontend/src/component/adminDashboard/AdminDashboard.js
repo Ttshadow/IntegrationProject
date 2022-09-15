10d256bb42840be58c3fd95d317b6d6703d61d79
JWSun123 <91690088+JWSun123@users.noreply.github.com>
2022-09-15 17:26:55 -0400

    change admindashboard layout with sidebar

--- frontend/src/component/adminDashboard/AdminDashboard.js
+++ frontend/src/component/adminDashboard/AdminDashboard.js
@@ -1,14 +1,22 @@
 import { Outlet } from "react-router-dom";
 import { MenuDashboard } from "./MenuDashboard";
 import SideBar from './SideBar';
+import { Col, Container, Row } from "react-bootstrap";
 
 function AdminDashboard() {
 
     return(
-        <>
-        <SideBar />
-        <Outlet />
-        </>
+        <Container>
+            <Row>
+                <Col className="col-3">
+                    <SideBar />
+                </Col>
+                <Col className="col-9">        
+                    <Outlet />
+                </Col>
+            </Row>
+            
+        </Container>
         
     )
 }
