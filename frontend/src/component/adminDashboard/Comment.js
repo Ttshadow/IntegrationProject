c6cbd4492daf8e95be153cc9fee4655f9f573225
carolinn_lyy <57008592+carolinn-lyy@users.noreply.github.com>
2022-09-16 17:16:14 -0400

    admin comment view

---
+++ frontend/src/component/adminDashboard/Comment.js
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from 'react';
+import {Table } from 'react-bootstrap';
+
+function Comment(){
+
+    const [comments, setComments] = useState([])
+
+    useEffect(() =>{
+        fetch('admindashboard/review', {
+            method: 'GET',
+        })
+        .then((data)=> data.json())
+        .then((json)=> {
+            console.log(json)
+            setComments(JSON.parse(JSON.stringify(json)))
+        });
+    }, []);
+
+    return(
+        <>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>User</th>
+                    <th>Review</th>
+                    <th>Date Created</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {comments.map((comment) =>{
+                        return <tr key={comment.id}>
+                            <td>{comment.user.firstName} {comment.user.lastName}</td>
+                            <td>{comment.content}</td>
+                            <td>{comment.createDate}</td>
+                            </tr>
+                    })}
+                    
+                </tbody>
+            </Table>
+        </>
+    )
+}
+
+export default Comment;
