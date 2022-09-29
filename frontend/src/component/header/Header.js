81869cd2abca9f0dcb2a6515841c0a905942b222
Ttshadow <91695920+Ttshadow@users.noreply.github.com>
2022-09-29 12:25:32 -0400

    responsive navbar

--- frontend/src/component/header/Header.js
+++ frontend/src/component/header/Header.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import { Col, Container } from "react-bootstrap";
 import { BsCartDash } from "react-icons/bs";
 import { FaUserCircle } from "react-icons/fa";
+import { FiMenu, FiX } from "react-icons/fi";
 import useLocalStorage from "../../util/useLocalStorage";
 
 export default function Header() {
   const [jwt, setJwt] = useLocalStorage("", "jwt");
   const [authority, setAuthority] = useLocalStorage("", "authority");
   const [userId, setUserId] = useLocalStorage("", "userId");
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(
+    window.matchMedia("(max-width: 768px)").matches
+  );
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setIsMobile(window.matchMedia("(max-width: 768px)").matches);
+    });
+  }, []);
   const handleLogin = () => {
     if (jwt) {
       setJwt("");
@@ -22,66 +33,87 @@ export default function Header() {
   };
 
   const jumpToDashboard = () => {
-    if(authority === "ROLE_ADMIN") {
+    if (authority === "ROLE_ADMIN") {
       window.location.href = "/admindashboard/table";
-    }else if(authority === "ROLE_USER") { 
+    } else if (authority === "ROLE_USER") {
       window.location.href = "/userdashboard/profile";
-    }else{
+    } else {
       window.location.href = "/";
     }
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Container>
-        <Col className="text-start">
-          <Link to="/">
-            <div id="logo"></div>
-          </Link>
-        </Col>
-
-        <Col className="collapse navbar-collapse " id="navbarSupportedContent">
-          <ul className="navbar-nav navbar mx-auto">
-            <li className="nav-item">
-              <Link to="dine" className="nav-link">
-                Menu
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link to="/newreservation" className="nav-link">
-                Reservation
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="">
-                Testimonials
-              </Link>
-            </li>
-          </ul>
-        </Col>
-
-        <Col>
-          <ul className="navbar-nav navbar float-end">
-            <li className="nav-item">
-              <button className="btn btn-light"onClick={()=>{window.location.href = "/dine/cart";}}>
-                <BsCartDash />
-              </button>
-              <span
-                className="position-absolute badge rounded-pill bg-danger"
-                id="cart-number"
-              ></span>
-              <button className="me-2 btn btn-light" onClick={jumpToDashboard}>
-                <FaUserCircle />
-              </button>
-              <button onClick={handleLogin} className='btn btn-light'>
-                {jwt ? "Log Out" : "Log In"}
-              </button>
-            </li>
-          </ul>
-        </Col>
-      </Container>
-    </nav>
+    <div className="nav-container">
+      <div className="navigation">
+        <Link to="/">
+          <div id="logo"></div>
+        </Link>
+        <div className="navMenu">
+          <nav className={isMobile && isNavOpen ? "open" : undefined}>
+            {isMobile && (
+              <div
+                className="closeIcon"
+                tabIndex="0"
+                role="button"
+                onClick={() => setIsNavOpen(false)}
+                onKeyDown={() => setIsNavOpen(false)}
+              >
+                <FiX />
+              </div>
+            )}
+            <ul>
+              <li>
+                <Link to="dine">
+                  Menu
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/newreservation">
+                  Reservation
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="">
+                  Testimonials
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          {isMobile && (
+            <div
+              className="menuIcon"
+              tabIndex="0"
+              role="button"
+              onClick={() => setIsNavOpen(true)}
+              onKeyDown={() => setIsNavOpen(true)}
+            >
+              <FiMenu />
+            </div>
+          )}
+        </div>
+        <ul>
+          <li>
+            <button
+              className="btn btn-light"
+              onClick={() => {
+                window.location.href = "/dine/cart";
+              }}
+            >
+              <BsCartDash />
+            </button>
+            <span
+              className="position-absolute badge rounded-pill bg-danger"
+              id="cart-number"
+            ></span>
+            <button className="me-2 btn btn-light" onClick={jumpToDashboard}>
+              <FaUserCircle />
+            </button>
+            <button onClick={handleLogin} className="btn btn-light">
+              {jwt ? "Log Out" : "Log In"}
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
